d9847b1e19b86d132464b5f8eb73ad6b8a484e71
Fernthedev <fernramirezgon@gmail.com>
2018-03-01 18:57:20 -0400

    ?

--- src/main/java/io/github/fernplayzz/fcommands/bungee.java
+++ src/main/java/io/github/fernplayzz/fcommands/bungee.java
@@ -1,4 +1,15 @@
 package io.github.fernplayzz.fcommands;
 
-public class bungee {
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class bungee extends Plugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("BUNGEECORD IS NOT SUPPORTED YET!");
+        getLogger().info("DISABLING PLUGIN!");
+        getProxy().getPluginManager().unregisterListeners(this);
+        getProxy().getPluginManager().unregisterCommands(this);
+        this.onDisable();
+    }
 }
--- src/main/java/io/github/fernplayzz/fcommands/spigotclass/BedFire.java
+++ src/main/java/io/github/fernplayzz/fcommands/spigotclass/BedFire.java
@@ -6,12 +6,14 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class BedFire extends JavaPlugin implements Listener {
-    public Player players = (Player) Bukkit.getServer().getOnlinePlayers();
-    for(Player p : Bukkit.getOnlinePlayers()){
+public class BedFire extends JavaPlugin {
+    //Player players = (Player) Bukkit.getServer().getOnlinePlayers();
+    for(Player players : Bukkit.getServer().getOnlinePlayers()) {
+
         if (players.isSleeping()) {
             players.setFireTicks(200);
             players.sendMessage(ChatColor.GOLD + "YOU'RE NOW ON FIRE THANKS TO A SHAODW! HAHAHAHAH");
         }
     }
 }
+
