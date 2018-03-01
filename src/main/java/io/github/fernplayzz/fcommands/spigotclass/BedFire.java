26af279bf09a3515231691a663540146111ed7f8
Fernthedev <fernramirezgon@gmail.com>
2018-03-01 19:10:15 -0400

    ?

--- src/main/java/io/github/fernplayzz/fcommands/spigotclass/BedFire.java
+++ src/main/java/io/github/fernplayzz/fcommands/spigotclass/BedFire.java
@@ -3,9 +3,10 @@ package io.github.fernplayzz.fcommands.spigotclass;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class BedFire extends JavaPlugin {
+public class BedFire extends JavaPlugin implements Listener {
     //Player players = (Player) Bukkit.getServer().getOnlinePlayers();
     public static void run() {
         for (Player players : Bukkit.getServer().getOnlinePlayers()) {
