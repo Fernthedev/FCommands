8c4160fa20a584feef835ce3cad94b48e876133f
Fernthedev <fernramirezgon@gmail.com>
2018-03-02 20:30:08 -0400

    ?

--- src/main/java/io/github/fernplayzz/fcommands/spigotclass/WaterBoots.java
+++ src/main/java/io/github/fernplayzz/fcommands/spigotclass/WaterBoots.java
@@ -5,8 +5,12 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+
+import java.util.ArrayList;
 
 public class WaterBoots implements Listener {
     //Another cool idea from Ender
@@ -31,6 +35,86 @@ public class WaterBoots implements Listener {
 
             }
         }
+
+        public static void bootrecipe() {
+            Bukkit.getLogger().info("LOADED RECIPES FOR WATERBOOTS");
+            // Our custom variable which we will be changing around.
+            ItemStack item = new ItemStack(Material.DIAMOND_BOOTS);
+            ItemStack itemiro = new ItemStack(Material.IRON_BOOTS);
+            ItemStack itemchai = new ItemStack(Material.CHAINMAIL_BOOTS);
+
+
+            // The meta of the diamond sword where we can change the name, and properties of the item.
+            ItemMeta meta = item.getItemMeta();
+            ItemMeta metairo = itemiro.getItemMeta();
+            ItemMeta metachai= itemchai.getItemMeta();
+
+            // This sets the name of the item.
+// Instead of the § symbol, you can use ChatColor.<color>
+            meta.setDisplayName(item.getItemMeta().getDisplayName());
+            metairo.setDisplayName(itemiro.getItemMeta().getDisplayName());
+            metachai.setDisplayName(itemchai.getItemMeta().getDisplayName());
+// Set the meta of the sword to the edited meta.
+            ///DIA
+            ItemMeta itemmeta = item.getItemMeta();
+            ArrayList<String> lore = new ArrayList<String>();
+            lore.add("WaterWalk");
+            itemmeta.setLore(lore);
+            //IRON
+            ItemMeta itemmetairo = itemiro.getItemMeta();
+            ArrayList<String> loreiro = new ArrayList<String>();
+            loreiro.add("WaterWalk");
+            itemmetairo.setLore(loreiro);
+
+            //CHAIN
+
+            ItemMeta itemmetachai = itemchai.getItemMeta();
+            ArrayList<String> lorechai = new ArrayList<String>();
+            lorechai.add("WaterWalk");
+            itemmetachai.setLore(lorechai);
+
+
+            // Create our custom recipe variable
+            //DIAMOND //////////////////////////////////////////////////
+            ShapedRecipe recipedia = new ShapedRecipe(item);
+// Here we will set the places. E and S can represent anything, and the letters can be anything. Beware; this is case sensitive.
+            recipedia.shape(" E ", " S ");
+
+// Set what the letters represent.
+            recipedia.setIngredient('E', Material.DIAMOND_BOOTS);
+            recipedia.setIngredient('S', Material.ICE);
+
+// Finally, add the recipe to the bukkit recipes
+            Bukkit.addRecipe(recipedia);
+            /////////////////////////////////////////////////////////////////
+            //IRON //////////////////////////////////////////////////////////
+            ShapedRecipe recipeiro = new ShapedRecipe(itemiro);
+
+// Here we will set the places. E and S can represent anything, and the letters can be anything. Beware; this is case sensitive.
+            recipeiro.shape(" E ", " S ");
+
+// Set what the letters represent.
+            recipeiro.setIngredient('E', Material.IRON_BOOTS);
+            recipeiro.setIngredient('S', Material.ICE);
+
+// Finally, add the recipe to the bukkit recipes
+            Bukkit.addRecipe(recipeiro);
+          //////////////////////////////////////////////////////////////
+            //CHAIN
+            ShapedRecipe recipechai = new ShapedRecipe(itemchai);
+
+// Here we will set the places. E and S can represent anything, and the letters can be anything. Beware; this is case sensitive.
+            recipeiro.shape(" E ", " S ");
+
+// Set what the letters represent.
+            recipechai.setIngredient('E', Material.IRON_BOOTS);
+            recipechai.setIngredient('S', Material.ICE);
+
+// Finally, add the recipe to the bukkit recipes
+            Bukkit.addRecipe(recipechai);
+
+        }
+
         public static void onEnable() {
         Bukkit.getLogger().info("WATER WALKING HAS BEEN ENABLED!");
         }
