f4480a3bcd9a50ab6ba497cb3587874a47f06f0c
Fernthedev <fernramirezgon@gmail.com>
2018-03-10 22:03:16 -0400

    PlayerRider improved, still bugs tho

--- src/main/java/io/github/fernplayzz/fcommands/bungee.java
+++ src/main/java/io/github/fernplayzz/fcommands/bungee.java
@@ -21,34 +21,31 @@ public class bungee extends Plugin {
    public void onEnable() {
        getLogger().info("ENABLED FERNCOMMANDS FOR BUNGEECORD");
       // getProxy().getPluginManager().registerListener(this, new servermaintenance());
-       servermaintenance();
+            boolean online = false;
+            try {
+                Socket s = new Socket();
+                s.connect(new InetSocketAddress("ADRESS", 25566), 20); //good timeout is 10-20
+                // ONLINE
+                //getProxy().getLogger().info("Lobby connected, disabling maintenance");
+                getProxy().getPluginManager().dispatchCommand(getProxy().getConsole(), "/maintenance off");
+                s.close();
+                //getProxy().getLogger().info("Lobby connected, disabling maintenance");
+                getProxy().getPluginManager().dispatchCommand(getProxy().getConsole(), "/maintenance off");
+                online = true;
+            } catch (UnknownHostException e) {
+                //getProxy().getLogger().info("Lobby disconnected, enabling maintenance");
+                getProxy().getPluginManager().dispatchCommand(getProxy().getConsole(), "/maintenance on");
+                // OFFLINE
+            } catch (IOException e) {
+                // OFFLINE
+                //getProxy().getLogger().info("Lobby disconnected, enabling maintenance");
+                getProxy().getPluginManager().dispatchCommand(getProxy().getConsole(), "/maintenance on");
+            }
     }
 
 
 
     public void servermaintenance() {
         getProxy().getLogger().info("LOADED LOBBY MAINTENANCE DETECTOR");
-        for(; ;) {
-            boolean online = false;
-            try {
-                Socket s = new Socket();
-                s.connect(new InetSocketAddress("ADRESS", 25566), 20); //good timeout is 10-20
-                // ONLINE
-                getProxy().getLogger().info("Lobby connected, disabling maintenance");
-                getProxy().getPluginManager().dispatchCommand(getProxy().getConsole(), "/maintenance off");
-                s.close();
-                getProxy().getLogger().info("Lobby connected, disabling maintenance");
-                getProxy().getPluginManager().dispatchCommand(getProxy().getConsole(), "/maintenance off");
-                online = true;
-            } catch (UnknownHostException e) {
-                getProxy().getLogger().info("Lobby disconnected, enabling maintenance");
-                getProxy().getPluginManager().dispatchCommand(getProxy().getConsole(), "/maintenance on");
-                // OFFLINE
-            } catch (IOException e) {
-                // OFFLINE
-                getProxy().getLogger().info("Lobby disconnected, enabling maintenance");
-                getProxy().getPluginManager().dispatchCommand(getProxy().getConsole(), "/maintenance on");
-            }
-        }
     }
 }
--- src/main/java/io/github/fernplayzz/fcommands/spigotclass/RidePlayer.java
+++ src/main/java/io/github/fernplayzz/fcommands/spigotclass/RidePlayer.java
@@ -17,32 +17,36 @@ public class RidePlayer implements Listener {
     @SuppressWarnings("deprecation")
     @EventHandler
     public static void onPlayerClick(PlayerInteractEntityEvent e) {
-        Player en = (Player) e.getRightClicked();
-        Player p = e.getPlayer();
-        Slime slime = (Slime) en.getWorld().spawnEntity(en.getLocation(), EntityType.SLIME);
-        slime.setSilent(true);
-        slime.setSize(2);
-        slime.setInvulnerable(true);
-        slime.setCollidable(false);
-        slime.setAI(false);
-        slime.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 9999999, 9999999, false, false));
-            if (e.getRightClicked().getType() == EntityType.PLAYER) {
-                en.addPassenger(slime);
-                slime.addPassenger(p);
-                if (en.getPassengers() != null) {
-                    for (Entity epl : en.getPassengers()) {
-                        Entity eppl = (Entity) epl.getPassengers();
-                        for (Player epplayer = (Player) eppl.getPassengers(); ; ) {
-                            epplayer.addPassenger(slime);
-                            slime.addPassenger(p);
-                            //epplayer.setPassenger(p);
-                        }
-                    }
-                    p.setPassenger(slime);
-                    en.setPassenger(p);
+        if (e.getRightClicked().getType() == EntityType.PLAYER) {
+            Player en = (Player) e.getRightClicked();
+            Player p = e.getPlayer();
+            Slime slime = (Slime) en.getWorld().spawnEntity(en.getLocation(), EntityType.SLIME);
+            if(slime.getVehicle() == en) {
+                e.setCancelled(true);
+            }
+            slime.setSilent(true);
+            slime.setSize(2);
+            slime.setInvulnerable(true);
+            slime.getNoDamageTicks();
+            slime.setCollidable(false);
+            slime.setAI(false);
+            slime.setHealth(900000000);
+            slime.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 9999999, 9999999, false, false));
+            en.addPassenger(slime);
+            slime.addPassenger(p);
+            if (en.getPassengers() != null) {
+                for (Entity epl : slime.getPassengers()) {
+                    Entity eppl = (Entity) epl.getPassengers();
+                    eppl.addPassenger(p);
+                       // e.addPassenger(slime);
+                       // slime.addPassenger(p);
+                        //epplayer.setPassenger(p);
                 }
+                p.setPassenger(slime);
+                en.setPassenger(p);
             }
         }
+    }
 
     public static void onEnable() {
         Bukkit.getLogger().info("LOADED RIDER CLASS");
@@ -50,17 +54,17 @@ public class RidePlayer implements Listener {
     @SuppressWarnings("deprecation")
     @EventHandler
     public static void onPlayerDismount(EntityDismountEvent e) {
-        Entity sl = e.getDismounted();
-        Player en = (Player) sl.getPassenger();
         Player p = (Player) e.getEntity();
-        Slime slime = (Slime) en.getWorld().spawnEntity(en.getLocation(), EntityType.SLIME);
-        slime.setSilent(true);
-        slime.setSize(2);
-        slime.setInvulnerable(true);
-        slime.setCollidable(false);
-        slime.setAI(false);
-        slime.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 9999999, 9999999, false, false));
-        slime.remove();
+        p.sendMessage(e.getDismounted() + " Was your vehicle");
+        Entity sl = e.getDismounted();
+        sl.eject();
+        sl.remove();
+        if(!sl.isDead()) {
+            p.sendMessage("slime still alive");
+        }
+        if(sl.getVehicle().getType() == EntityType.PLAYER) {
+            p.sendMessage("slime didnt dismount");
+        }
     }
 }
 
