9dad26625637156cebd6aa9b01ea30cfa60b0240
Fernthedev <fernramirezgon@gmail.com>
2018-04-05 20:35:32 -0400

    Fixed mcMMO and Nametagedit not working. (when mcmmo scoreboard showed up nametags got reset)

---
+++ src/main/java/io/github/fernplayzz/fcommands/spigotclass/NTEmcMMO.java
@@ -0,0 +1,27 @@
+package io.github.fernplayzz.fcommands.spigotclass;
+
+import com.gmail.nossr50.events.experience.McMMOPlayerLevelUpEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class NTEmcMMO implements Listener {
+
+    @SuppressWarnings("privatation")
+    public static void ntereload() {
+        Bukkit.getServer().getLogger().info("Reloading nte due to levelup from mcmmo");
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(),"/nte reload");
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(),"nte reload");
+    }
+    //@EventHandler
+   // public void levelup(McMMOPlayerLevelUpEvent upe) {
+   //     ntereload();
+   // }
+    @EventHandler
+    public static void onLevelChange(final McMMOPlayerLevelUpEvent change) {
+        Bukkit.getServer().getLogger().info("Player leveled up, reloading nametags");
+        ntereload();
+    }
+
+
+}
