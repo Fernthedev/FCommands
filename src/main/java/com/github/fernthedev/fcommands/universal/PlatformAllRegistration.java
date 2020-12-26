52213cf280cca559e744f8dd21507b28527e49be
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2020-12-26 12:21:38 -0400

    Improve classpath compatibility

--- src/main/java/com/github/fernthedev/fcommands/universal/PlatformAllRegistration.java
+++ src/main/java/com/github/fernthedev/fcommands/universal/PlatformAllRegistration.java
@@ -3,7 +3,8 @@ package com.github.fernthedev.fcommands.universal;
 import com.github.fernthedev.fcommands.universal.commands.DebugCommand;
 import com.github.fernthedev.fcommands.universal.commands.UFernPing;
 import com.github.fernthedev.fernapi.universal.Universal;
-import com.github.fernthedev.preferences.core.PreferenceManager;
+
+import java.lang.reflect.InvocationTargetException;
 
 public class PlatformAllRegistration {
 
@@ -13,7 +14,17 @@ public class PlatformAllRegistration {
         Universal.getCommandHandler().registerCommand(new DebugCommand());
 
         if (Universal.getMethods().getServerType().isProxy()) {
-            PreferenceManager.registerPreference(new PluginPreferenceManager());
+
+            try {
+                // Use reflection to avoid classpath issues with Spigot
+                Class<?> aClass = Class.forName("com.github.fernthedev.preferences.core.PreferenceManager");
+
+                Class<?> preferenceClass = Class.forName("com.github.fernthedev.preferences.core.PluginPreference");
+
+                aClass.getMethod("registerPreference", preferenceClass).invoke(null, new PluginPreferenceManager());
+            } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
         }
 
 
