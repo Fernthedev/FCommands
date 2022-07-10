28964caae0141cbca55a39a33c33240bfa80080b
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-07-09 23:30:08 -0400

    fix jank

--- src/main/java/com/github/fernthedev/fcommands/bungee/modules/BungeeEventsModule.kt
+++ src/main/java/com/github/fernthedev/fcommands/bungee/modules/BungeeEventsModule.kt
@@ -1,27 +1,16 @@
 package com.github.fernthedev.fcommands.bungee.modules
 
 import com.github.fernthedev.fcommands.bungee.BungeeEvents
+import com.github.fernthedev.fcommands.proxy.ProxyEvents
+import com.github.fernthedev.fcommands.toType
 import com.github.fernthedev.fcommands.universal.BaseEvents
-import com.github.fernthedev.fernapi.universal.Universal
 import com.google.inject.AbstractModule
 
 class BungeeEventsModule : AbstractModule() {
 
     override fun configure() {
-        val classes = mutableListOf<Class<out BaseEvents>>(BungeeEvents::class.java)
-
-
-        var parentKlass: Class<*>? = BungeeEvents::class.java.superclass;
-        while (parentKlass != null && BungeeEvents::class.java.isAssignableFrom(parentKlass)) {
-            classes.add(parentKlass as Class<out BaseEvents>)
-            parentKlass = parentKlass.superclass
-        }
-
-        val typeLiteral: Class<out Nothing> = BungeeEvents::class.java.asSubclass(Nothing::class.java);
-
-        classes.forEach { clazz ->
-            Universal.getLogger().info("Registering ${clazz.toGenericString()}")
-            bind(clazz).to(typeLiteral)
-        }
+        bind(BungeeEvents::class.java)
+        bind(ProxyEvents::class.java).toType(BungeeEvents::class.java)
+        bind(BaseEvents::class.java).toType(BungeeEvents::class.java)
     }
 }
--- src/main/java/com/github/fernthedev/fcommands/velocity/modules/VelocityEventsModule.kt
+++ src/main/java/com/github/fernthedev/fcommands/velocity/modules/VelocityEventsModule.kt
@@ -1,5 +1,7 @@
 package com.github.fernthedev.fcommands.velocity.modules
 
+import com.github.fernthedev.fcommands.proxy.ProxyEvents
+import com.github.fernthedev.fcommands.toType
 import com.github.fernthedev.fcommands.universal.BaseEvents
 import com.github.fernthedev.fcommands.velocity.VelocityEvents
 import com.google.inject.AbstractModule
@@ -7,19 +9,8 @@ import com.google.inject.AbstractModule
 class VelocityEventsModule : AbstractModule() {
 
     override fun configure() {
-        val classes = mutableListOf<Class<out BaseEvents>>(VelocityEvents::class.java)
-
-
-        var parentKlass: Class<*>? = VelocityEvents::class.java.superclass;
-        while (parentKlass != null && VelocityEvents::class.java.isAssignableFrom(parentKlass)) {
-            classes.add(parentKlass as Class<out BaseEvents>)
-            parentKlass = parentKlass.superclass
-        }
-
-        val typeLiteral: Class<out Nothing> = VelocityEvents::class.java.asSubclass(Nothing::class.java);
-
-        classes.forEach { clazz ->
-            bind(clazz).to(typeLiteral)
-        }
+        bind(VelocityEvents::class.java)
+        bind(ProxyEvents::class.java).toType(VelocityEvents::class.java)
+        bind(BaseEvents::class.java).toType(VelocityEvents::class.java)
     }
 }
