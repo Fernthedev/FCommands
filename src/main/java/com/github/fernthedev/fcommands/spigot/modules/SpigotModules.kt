1af6240622a5c546c5001d9bc88ad02f8e9293d0
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2023-01-09 11:12:56 -0400

    Fixed startup

--- src/main/java/com/github/fernthedev/fcommands/spigot/modules/SpigotModules.kt
+++ src/main/java/com/github/fernthedev/fcommands/spigot/modules/SpigotModules.kt
@@ -3,9 +3,7 @@ package com.github.fernthedev.fcommands.spigot.modules
 import com.github.fernthedev.fcommands.spigot.FernCommands
 import com.google.inject.AbstractModule
 import com.google.inject.util.Providers
-import org.bukkit.Server
 import org.bukkit.configuration.file.FileConfiguration
-import org.bukkit.plugin.Plugin
 
 class SpigotModules(
     val plugin: FernCommands
@@ -14,14 +12,9 @@ class SpigotModules(
     override fun configure() {
         // Use providers so the instance itself
         // doesn't have injectMembers called on it
-        bind(Plugin::class.java)
-            .toProvider(Providers.of(plugin))
-
         bind(FernCommands::class.java)
             .toProvider(Providers.of(plugin))
 
-        bind(Server::class.java)
-            .toProvider(Providers.of(plugin.server))
 
         bind(FileConfiguration::class.java)
             .toProvider(Providers.of(plugin.config))
